203e709f15cbb27584a05a6b536aebd7a9837165
wb-zhangchenchen <wb-zhangchenchen@alibaba-inc.com>
2016-04-01 17:19:39 +0800

    ok

--- ppm/src/main/java/com/alibaba/ppm/home/module/screen/Homepage.java
+++ ppm/src/main/java/com/alibaba/ppm/home/module/screen/Homepage.java
@@ -1,19 +1,12 @@
 package com.alibaba.ppm.home.module.screen;
 
-import java.io.InputStream;
 import java.util.List;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.citrus.turbine.Context;
 import com.alibaba.ppm.home.module.dal.bo.ProjectBo;
 import com.alibaba.ppm.home.module.dal.daoobject.ProjectBean;
-import com.alibaba.ppm.home.module.dal.mapper.ProjectBeanMapper;
-import com.alibaba.ppm.home.module.dal.mapper.ProjectBeanMapperExt;
 
 public class Homepage {
 	@Autowired
